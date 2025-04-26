8ed20c63d8e6c9b10edf7bf14467d21142b0d37f
leandro.lupano <leandro.lupano@satec.es>
2025-04-26 22:32:23 +0200

    Initial commit

---
+++ src/main/java/com/inditex/productsort/ProductSortingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.inditex.productsort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSortingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductSortingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inditex/productsort/ProductSortingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inditex.productsort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSortingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
